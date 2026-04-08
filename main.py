85e90e17228644c1f23290f6de3f4b39c726f033
Yashas422005 <yashas422005@gmail.com>
2026-04-08 13:02:22 +0530

    LLM Integration Completed

--- main.py
+++ main.py
@@ -1,9 +1,119 @@
 from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.agents import create_agent
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+tavily_api_key = os.getenv("TAVILY_API_KEY")
 
 llm = ChatOpenAI(
     model="openai/gpt-oss-20b",
     base_url="https://api.groq.com/openai/v1",
-    api_key="GROQ_API_KEY",
+    api_key=os.getenv("GROQ_API_KEY"),
     temperature=0,
 )
+
+search_engine = TavilySearchResults(api_key=tavily_api_key, num_results=3)
+tools = [search_engine]
+
+
+
+"""
+You are an expert agricultural advisor for sugarcane crops in India.
+
+Input:
+- Detected diseases (from: ["Rust", "Red Rot", "Yellow", "Bacterial Blight", "Mosaic", "Healthy"])
+- Recommended chemicals with dosage (already provided)
+
+Task:
+Convert the given recommendations into a clear and concise treatment summary.
+
+Rules:
+- If disease = "Healthy", output exactly:
+  "No treatment needed. Crop is healthy."
+- Otherwise:
+  - Provide ONLY a 3-line summary.
+  - Each line must include:
+    1. Chemical name
+    2. Given dosage (DO NOT modify it)
+    3. Simple usage instruction (spray/method)
+- Do NOT add new chemicals or change dosage.
+- Do NOT include explanations or extra text.
+- Keep language simple and practical for farmers.
+- Maximum 3 lines only.
+
+Example Input:
+Diseases: ["Rust", "Bacterial Blight"]
+Chemicals:
+- Propiconazole (0.1%)
+- Mancozeb (0.25%)
+- Streptocycline (100 ppm) + Copper oxychloride (0.3%)
+
+Example Output:
+1. Propiconazole (0.1%) spray on leaves to control rust infection.
+2. Mancozeb (0.25%) apply as foliar spray at regular intervals.
+3. Streptocycline (100 ppm) + Copper oxychloride (0.3%) spray to manage bacterial blight.
+"""
+
+
+from src.image_model import image_model_pipeline
+
+top3_labels = image_model_pipeline()
+
+if "Healthy" in top3_labels:
+    top3_labels.remove("Healthy")
+
+input=",".join(top3_labels)
+chemicals = '0.002 ppm'
+
+from langchain_core.prompts import PromptTemplate
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", 
+     """You are an expert agricultural advisor for sugarcane crops in India.
+
+Rules:
+- If disease = Healthy → "No treatment needed. Crop is healthy."
+- Otherwise → ONLY 3 lines
+- Use given dosage only
+- No extra text
+- Provide chemicals relevant to the diseases mentioned in the input.
+"""),
+
+    ("human", 
+     """Diseases: {input}
+Dosage: {chemicals}
+Additionally , Also provide some relevant chemicals for the diseases.
+""")
+])
+
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    system_prompt="""You are an expert agricultural advisor for sugarcane crops in India.
+
+Rules:
+- If disease = Healthy → "No treatment needed. Crop is healthy."
+- Otherwise → ONLY 3 lines
+- Use given dosage only
+- No extra text
+"""
+)
+from langchain.messages import HumanMessage
+
+response = agent.invoke({
+    "messages":[HumanMessage(content=f"""Diseases: {input}
+Dosage: {chemicals}
+
+Provide general treatment recommendations in general for the diseases mentioned in input.
+
+
+""")
+]
+})
+print(response['messages'][1].content)
+
+
 
--- src/image_model.py
+++ src/image_model.py
@@ -6,48 +6,50 @@ from matplotlib.image import imread
 from tensorflow.keras.preprocessing import image
 import os 
 
-os.chdir("../")
+
 
 from tensorflow.keras import models, layers, applications
 
-base = applications.EfficientNetB0(
-    include_top=False,
-    weights='imagenet',
-    input_shape=(224, 224, 3),
-    pooling='avg'
-)
+def image_model_pipeline():
+    base = applications.EfficientNetB0(
+        include_top=False,
+        weights='imagenet',
+        input_shape=(224, 224, 3),
+        pooling='avg'
+    )
 
-inputs = layers.Input(shape=(224, 224, 3))
-x = base(inputs, training=False)
-x = layers.Dropout(0.3)(x)
-x = layers.Dense(256, activation='relu')(x)
-x = layers.BatchNormalization()(x)
-outputs = layers.Dense(6, activation='softmax')(x)
-model = models.Model(inputs, outputs)
+    inputs = layers.Input(shape=(224, 224, 3))
+    x = base(inputs, training=False)
+    x = layers.Dropout(0.3)(x)
+    x = layers.Dense(256, activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+    outputs = layers.Dense(6, activation='softmax')(x)
+    model = models.Model(inputs, outputs)
 
 
-model.load_weights("dominator.h5")
+    model.load_weights("dominator.h5")
 
-my_image = image.load_img('C://Users//Yashas/img16.jpg',target_size=(224,224,3))
+    my_image = image.load_img('C://Users//Yashas/img16.jpg',target_size=(224,224,3))
 
-my_image = image.img_to_array(my_image)
+    my_image = image.img_to_array(my_image)
 
-my_image = np.expand_dims(my_image, axis=0)
+    my_image = np.expand_dims(my_image, axis=0)
 
-pred=model.predict(my_image)
+    pred=model.predict(my_image)
 
-class_idx = np.argmax(pred)
+    class_idx = np.argmax(pred)
 
-label_dict = {0:'BacterialBlights',
-1:'Healthy',
-2:'Mosaic',
-3:'RedRot',
-4:'Rust',
-5:'Yellow'}
+    label_dict = {0:'BacterialBlights',
+    1:'Healthy',
+    2:'Mosaic',
+    3:'RedRot',
+    4:'Rust',
+    5:'Yellow'}
 
-pred = model.predict(my_image)
-pred_sorted = pred.argsort(axis=1)
-top3_indices = pred_sorted[0, -1:-4:-1]  
-top3_labels = [label_dict[i] for i in top3_indices] 
+    pred = model.predict(my_image)
+    pred_sorted = pred.argsort(axis=1)
+    top3_indices = pred_sorted[0, -1:-4:-1]  
+    top3_labels = [label_dict[i] for i in top3_indices] 
 
-print(top3_labels)
+    return top3_labels
+
